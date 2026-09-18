8b5c291c82bd9e7f84defde2b4af459ddbf8d00e
muradvcv <muradvcv@gmail.com>
2026-09-18 22:46:49 +0600

    dsa solution done

---
+++ fazesoft-coding-test/DSA/solution.js
@@ -0,0 +1,20 @@
+ function jmNum(nums) {
+  // initial value
+  let maxnum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxnum) {
+      return false;
+    }
+
+    maxnum= Math.max(maxnum, i + nums[i]);
+
+    if (maxnum >= nums.length - 1) {
+      return true;
+    }
+  }
+
+  return true;
+}
+
+console.log(jmNum([1,2,3,5,6]));
